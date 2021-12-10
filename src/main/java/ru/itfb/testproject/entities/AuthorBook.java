fc05864c4bc0e9db4dee4dce2e1b28bf8fb68a6a
Aleksei Iagnenkov <a.iagnenkov@itfbgroup.ru>
2021-12-10 15:03:27 +0300

    link table created

---
+++ src/main/java/ru/itfb/testproject/entities/AuthorBook.java
@@ -0,0 +1,36 @@
+package ru.itfb.testproject.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema="json", name="author_book")
+public class AuthorBook {
+
+    @Id
+    @GeneratedValue
+    private Long pk;
+    private long author_id;
+    private long book_id;
+
+    public AuthorBook() {
+    }
+
+    public long getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(long author_id) {
+        this.author_id = author_id;
+    }
+
+    public long getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(long book_id) {
+        this.book_id = book_id;
+    }
+}
