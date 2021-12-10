f352e576ec997f8e88a436b9fbf5f8343d13cb44
Aleksei Iagnenkov <a.iagnenkov@itfbgroup.ru>
2021-12-10 17:49:18 +0300

    nothing

--- src/main/java/ru/itfb/testproject/repositories/AuthorBookRepository.java
+++ src/main/java/ru/itfb/testproject/repositories/AuthorBookRepository.java
@@ -1,7 +1,7 @@
 package ru.itfb.testproject.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-import ru.itfb.testproject.entities.AuthorBook;
+import ru.itfb.testproject.models.AuthorBook;
 
 public interface AuthorBookRepository extends CrudRepository<AuthorBook, Long> {
 }
--- src/main/java/ru/itfb/testproject/repositories/AuthorRepository.java
+++ src/main/java/ru/itfb/testproject/repositories/AuthorRepository.java
@@ -1,7 +1,7 @@
 package ru.itfb.testproject.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-import ru.itfb.testproject.entities.Author;
+import ru.itfb.testproject.models.Author;
 
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
