b46ebaa6bc67d4fa953c8b50a2d33848b45fa916
Aleksei Iagnenkov <a.iagnenkov@itfbgroup.ru>
2021-12-14 12:01:13 +0300

    remove logout

--- src/main/java/ru/itfb/testproject/config/WebSecurityConfig.java
+++ src/main/java/ru/itfb/testproject/config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/home", "/books", "/book", "/authors", "/author", "/about", "/logout").permitAll()
+                    .antMatchers("/", "/home", "/books", "/book", "/authors", "/author", "/about").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
@@ -34,7 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails user =
                 User.withDefaultPasswordEncoder()
                         .username("admin")
-                        .password("0000")
+                        .password("admin")
                         .roles("USER")
                         .build();
 
