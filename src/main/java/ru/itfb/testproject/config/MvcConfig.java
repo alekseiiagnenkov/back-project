00a9a1dbcc9cfa7d30527858baf4dbc363730a9f
Aleksei Iagnenkov <a.iagnenkov@itfbgroup.ru>
2021-12-14 12:01:33 +0300

    little changes

--- src/main/java/ru/itfb/testproject/config/MvcConfig.java
+++ src/main/java/ru/itfb/testproject/config/MvcConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/logout").setViewName("logout");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/logout").setViewName("logout");
     }
 
 }
