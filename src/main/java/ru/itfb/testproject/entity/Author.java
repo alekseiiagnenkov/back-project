8f52a04d350125c46e70471d37efe85ee7f0cc64
Aleksei Iagnenkov <a.iagnenkov@itfbgroup.ru>
2021-12-16 18:25:10 +0300

    started use @Getters @Setters @ToString

--- src/main/java/ru/itfb/testproject/entity/Author.java
+++ src/main/java/ru/itfb/testproject/entity/Author.java
@@ -1,56 +1,28 @@
 package ru.itfb.testproject.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(schema="json", name="authors")
+@Table(name = "authors")
+@Getter
+@Setter
+@ToString
 public class Author {
 
     @Id
     @GeneratedValue
     private Long pk;
-
     private long id;
     private String firstName;
     private String lastName;
 
-
-
     public Author() {
-    }
-
-    public Long getPk() {
-        return pk;
-    }
-
-    public void setPk(Long pk) {
-        this.pk = pk;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 }
--- src/main/java/ru/itfb/testproject/entity/Book.java
+++ src/main/java/ru/itfb/testproject/entity/Book.java
@@ -1,55 +1,29 @@
 package ru.itfb.testproject.entity;
 
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(schema="json", name="books")
+@Table(name = "books")
+@Getter
+@Setter
+@ToString
 public class Book {
 
     @Id
     @GeneratedValue
     private Long pk;
-
     private long id;
     private String name;
     private String author;
 
     public Book() {
-    }
-
-    public Long getPk() {
-        return pk;
-    }
-
-    public void setPk(Long pk) {
-        this.pk = pk;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
     }
 }
