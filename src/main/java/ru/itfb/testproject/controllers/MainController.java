906737308d0489d47c585cae83505721cbcb0f1a
Aleksei Iagnenkov <a.iagnenkov@itfbgroup.ru>
2021-12-16 18:23:46 +0300

    remove AuthorBook and started use Slf4j

--- src/main/java/ru/itfb/testproject/controllers/MainController.java
+++ src/main/java/ru/itfb/testproject/controllers/MainController.java
@@ -2,6 +2,7 @@ package ru.itfb.testproject.controllers;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.itfb.testproject.entity.Author;
-import ru.itfb.testproject.entity.AuthorBook;
 import ru.itfb.testproject.entity.Book;
-import ru.itfb.testproject.repositories.AuthorBookRepository;
 import ru.itfb.testproject.repositories.AuthorRepository;
 import ru.itfb.testproject.repositories.BookRepository;
 
@@ -22,23 +21,19 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
+@Slf4j
 @Controller
 public class MainController {
 
-    private final static Logger logger = LoggerFactory.getLogger(MainController.class);
-
     private BookRepository bookRepository;
     private AuthorRepository authorRepository;
-    private AuthorBookRepository authorBookRepository;
 
     @Autowired
-    public MainController(BookRepository bookRepository, AuthorRepository authorRepository, AuthorBookRepository authorBookRepository) {
+    public MainController(BookRepository bookRepository, AuthorRepository authorRepository) {
         this.bookRepository = bookRepository;
         this.authorRepository = authorRepository;
-        this.authorBookRepository = authorBookRepository;
         jsonBooks();
         jsonAuthors();
-        jsonAuthorBook();
     }
 
     @GetMapping("/")
@@ -46,10 +41,10 @@ public class MainController {
         return "main";
     }
 
-    @GetMapping("/login")
+/*    @GetMapping("/login") //TODO исправить ошибку 404
     public String login(Model model) {
         return "login";
-    }
+    }*/
 
     @GetMapping("/about")
     public String about(Model model) {
@@ -57,7 +52,7 @@ public class MainController {
     }
 
     @GetMapping("books")
-    public String books(Model model){
+    public String books(Model model) {
         Iterable<Book> books = bookRepository.findAll();
         model.addAttribute("books", books);
         return "books";
@@ -65,10 +60,10 @@ public class MainController {
 
     @GetMapping("/book")
     @RequestMapping(value = "/toBook", method = RequestMethod.POST)
-    public String book(Model model, @RequestParam(name = "Pk") Long Pk){
+    public String book(Model model, @RequestParam(name = "Pk") Long Pk) {
         Iterator<Book> books = bookRepository.findAll().iterator();
         Book book = books.next();
-        while (!Objects.equals(book.getPk(), Pk)){
+        while (!Objects.equals(book.getPk(), Pk)) {
             book = books.next();
         }
         model.addAttribute("book", book);
@@ -77,10 +72,10 @@ public class MainController {
 
     @GetMapping("/author")
     @RequestMapping(value = "/toAuthor", method = RequestMethod.POST)
-    public String author(Model model, @RequestParam(name = "Pk") Long Pk){
+    public String author(Model model, @RequestParam(name = "Pk") Long Pk) {
         Iterator<Author> authors = authorRepository.findAll().iterator();
         Author author = authors.next();
-        while (!Objects.equals(author.getPk(), Pk)){
+        while (!Objects.equals(author.getPk(), Pk)) {
             author = authors.next();
         }
         model.addAttribute("author", author);
@@ -97,84 +92,56 @@ public class MainController {
     @RequestMapping("jsonAuthors")
     @ResponseBody
     public void jsonAuthors() {
-        URL url = this.getClass().getClassLoader().getResource("authors.json");
+        URL url = this.getClass().getClassLoader().getResource("database/authors.json");
 
-        if(url != null){
+        if (url != null) {
 
             File jsonFile = new File(url.getFile());
 
             ObjectMapper objectMapper = new ObjectMapper();
 
             try {
-                List<Author> authors = objectMapper.readValue(jsonFile, new TypeReference<>(){
+                List<Author> authors = objectMapper.readValue(jsonFile, new TypeReference<>() {
 
                 });
 
                 authorRepository.saveAll(authors);
 
-                logger.info("All records saved.");
+                log.info("All records saved.");
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else {
-            logger.warn("uri is null.");
-        }
-    }
-
-    @RequestMapping("jsonAuthorBook")
-    @ResponseBody
-    public void jsonAuthorBook() {
-        URL url = this.getClass().getClassLoader().getResource("author_book.json");
-
-        if(url != null){
-
-            File jsonFile = new File(url.getFile());
-
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            try {
-                List<AuthorBook> authorBook = objectMapper.readValue(jsonFile, new TypeReference<>(){
-
-                });
-
-                authorBookRepository.saveAll(authorBook);
-
-                logger.info("All records saved.");
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            logger.warn("uri is null.");
+            log.warn("uri is null.");
         }
     }
 
     @RequestMapping("jsonBooks")
     @ResponseBody
-    public void jsonBooks(){
-        URL url = this.getClass().getClassLoader().getResource("books.json");
+    public void jsonBooks() {
+        URL url = this.getClass().getClassLoader().getResource("database/books.json");
 
-        if(url != null){
+        if (url != null) {
 
             File jsonFile = new File(url.getFile());
 
             ObjectMapper objectMapper = new ObjectMapper();
 
             try {
-                List<Book> books = objectMapper.readValue(jsonFile, new TypeReference<>(){
+                List<Book> books = objectMapper.readValue(jsonFile, new TypeReference<>() {
 
                 });
 
                 bookRepository.saveAll(books);
 
-                logger.info("All records saved.");
+                log.info("All records saved.");
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else {
-            logger.warn("uri is null.");
+            log.warn("uri is null.");
         }
 
 
