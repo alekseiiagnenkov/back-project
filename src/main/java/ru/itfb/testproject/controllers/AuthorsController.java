170d7e03badc6b94bbbf0528d2330e29103eed52
Aleksei Iagnenkov <a.iagnenkov@itfbgroup.ru>
2021-12-16 15:41:56 +0300

    Rest returning JSON

---
+++ src/main/java/ru/itfb/testproject/controllers/AuthorsController.java
@@ -0,0 +1,74 @@
+package ru.itfb.testproject.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import ru.itfb.testproject.exceptions.NotFoundException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("authorsJSON")
+public class AuthorsController {
+
+    private int counter = 4;
+
+    private List<Map<String, String>> authors = new ArrayList<>() {{
+        add(new HashMap<String, String>() {{
+            put("id", "1");
+            put("firstName", "Lev");
+            put("lastName", "Tolstoy");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "2");
+            put("firstName", "Evgeny");
+            put("lastName", "Petrov");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "3");
+            put("firstName", "Ilya");
+            put("lastName", "Ilf");
+        }});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> authors() {
+        return authors;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getAuthor(id);
+    }
+
+    private Map<String, String> getAuthor(String id) {
+        return authors.stream()
+                .filter(author -> author.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> author) {
+        author.put("id", String.valueOf(counter++));
+        authors.add(author);
+        return author;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> author) {
+        Map<String, String> authorFromDB = getAuthor(id);
+
+        authorFromDB.putAll(author);
+        authorFromDB.put("id", id);
+        return authorFromDB;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> author = getAuthor("id");
+        authors.remove(author);
+    }
+}
+
---
+++ src/main/java/ru/itfb/testproject/controllers/BookController.java
@@ -0,0 +1,68 @@
+package ru.itfb.testproject.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import ru.itfb.testproject.exceptions.NotFoundException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("booksJSON")
+public class BookController {
+
+    private int counter = 3;
+
+    private List<Map<String, String>> books = new ArrayList<>() {{
+        add(new HashMap<String, String>() {{
+            put("id", "1");
+            put("name", "12 chairs");
+            put("author", "Evgeny Petrov");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "2");
+            put("name", "War and Peace");
+            put("author", "Lev Tolstoy");
+        }});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> books() {
+        return books;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getBook(id);
+    }
+
+    private Map<String, String> getBook(String id) {
+        return books.stream()
+                .filter(book -> book.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> book) {
+        book.put("id", String.valueOf(counter++));
+        books.add(book);
+        return book;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> book) {
+        Map<String, String> bookFromDB = getBook(id);
+
+        bookFromDB.putAll(book);
+        bookFromDB.put("id", id);
+        return bookFromDB;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> book = getBook("id");
+        books.remove(book);
+    }
+}
