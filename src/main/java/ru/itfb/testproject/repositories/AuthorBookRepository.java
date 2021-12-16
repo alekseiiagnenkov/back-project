77334b0ef68725d2c9e6f012e4fb0d841700f0bd
Aleksei Iagnenkov <a.iagnenkov@itfbgroup.ru>
2021-12-16 18:21:55 +0300

    removed

--- src/main/java/ru/itfb/testproject/repositories/AuthorBookRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.itfb.testproject.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.itfb.testproject.entity.AuthorBook;
-
-public interface AuthorBookRepository extends CrudRepository<AuthorBook, Long> {
-}
