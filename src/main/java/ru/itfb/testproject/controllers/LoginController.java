95df2f6edbd40b62c0b3b676eef7aa85bef17a75
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2022-01-18 11:17:12 +0300

    created LoginController for auth

---
+++ src/main/java/ru/itfb/testproject/controllers/LoginController.java
@@ -0,0 +1,40 @@
+package ru.itfb.testproject.controllers;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import ru.itfb.testproject.entity.Person;
+
+/**
+ * Контроллер авторизации
+ */
+@SessionAttributes({"currentUser"})
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(SessionStatus session) {
+        SecurityContextHolder.getContext().setAuthentication(null);
+        session.setComplete();
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/postLogin", method = RequestMethod.POST)
+    public String postLogin() {
+        return "redirect:/";
+    }
+
+    private void validatePrinciple(Object principal) {
+        if (!(principal instanceof Person)) {
+            throw new  IllegalArgumentException("Principal can not be null!");
+        }
+    }
+}
